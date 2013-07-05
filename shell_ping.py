6dde558b7a7ba4320b4a1de37b69c6e1ad41d83f
Roger Yan <roger@localet.com>
2013-07-05 17:03:00 +0900

    fix AGAIN for "packet loss" parsing in shell_ping library

--- shell_ping.py
+++ shell_ping.py
@@ -63,6 +63,8 @@ def ping(target, times=4):
 
     # packet loss rate
     lost = re.findall(r'\d+.\d+(?=%)', text)
+    if not lost:
+        lost = re.findall(r'\d+(?=%)', text)
     lost = int(round(float(lost[len(lost)-1]))) if lost else 100
 
     return ip, time_min, time_avg, time_max, lost
